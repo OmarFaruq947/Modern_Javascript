fcec629ef9b1294ea0362c2278857b5ce7261565
MD.Omar Faruq <omarfaruq.bplus.6600@gmail.com>
2021-07-30 10:53:11 +0600

    arrowFunction

---
+++ FatArrowFunction.js
@@ -0,0 +1,29 @@
+function xy() {
+    return 10;
+}
+console.log(xy());
+
+
+
+var arrow = () => {
+    return 20
+}
+console.log(arrow());
+
+
+
+var arrow = () => 30
+console.log(arrow());
+
+
+
+
+var arrow = (x,y) => x+y
+console.log(arrow(15,20));
+
+
+
+
+
+
+
